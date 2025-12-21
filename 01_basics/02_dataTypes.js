0bbf3f12812b9426a8a6adfb1b1da028b1a69b89
Debashis Patra <debashispatra0992@gmail.com>
2025-12-21 23:04:10 +0530

    2nd Class Code.
    Upload 2st class code, the class is all about the java script data types.

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+
+"use strict"; // treat all JS code as newer version
+
+
+//##Primitive Data Types
+let stringData = "hitesh"                    //Strings
+let numberData = 18                          //Numbers
+let booleanData = false                      //Boolean
+let bigintData = 1234567890123456789012345n; //Bigint
+let undefinedData;                           // Undefined
+let nullData = null;                         //Null
+const symbolData = Symbol();                 // Symbol
+
+
+//##Non-Primitive Data Types (Oblect, Array, Function, Date, RegExp, Set, Map)
+const person = {firstName:"John", lastName:"Doe"};  // Object
+const cars = ["Saab", "Volvo", "BMW"];              // Array Object
+const date = new Date("2022-03-25");                // Date Object
+
+
+/*
+Number -- A number representing a mathematical value
+Bigint -- A number representing a large integer
+String -- A text of characters enclosed in quotes
+Boolean -- A data type representing true or false
+Undefined -- A primitive variable with no assigned value
+Null -- A primitive value representing object absence
+Symbol -- A unique and primitive identifier
+Object -- A collection of key-value pairs of data
+*/
+
+
+
+
+
+
